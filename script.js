1cd40691496c0781e4b8b9a812a698a6d10768a2
Aparna Rajeev <57600509+parna86@users.noreply.github.com>
2021-04-26 00:19:36 +0400

    colour changes

--- script.js
+++ script.js
@@ -26,18 +26,20 @@ for(let i = 0; i < listObjects.length; i++){
     console.log('For ', element.innerHTML);
     console.log('Angle ', (i*90));
     console.log("Top: ",top, ", Left: ", left);
+
+    
 }
 
 setInterval(()=>{
     for(let i = 0; i < listDegrees.length; i++){
-        listDegrees[i] += 0.25;
+        listDegrees[i] += 0.20;
         element = listObjects[i];
         let top = k + (r*Math.sin(degree_to_radians(listDegrees[i])));
         let left = h + (r*Math.cos(degree_to_radians(listDegrees[i])));
         element.style.top = top;
         element.style.left = left;
     }
-}, 50);
+}, 25);
 
 function degree_to_radians(deg){
     var pi = Math.PI;
